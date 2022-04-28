697242356ec2a2ea9363b9606e26e91d8fdc5109
Tero Salminen <tero.salminen@unikie.com>
2022-04-28 10:25:58 +0300

    ree_comm: implement component code in library
    All implementation moved to separate library in order to activate
    '-Wextra' compilation flag.
    
    Camkes component name and component instance name should be same
    after lowercase conversion (i.e. ComponentName vs componentname).
    Then camkes.h can be included from "${CMAKE_CURRENT_BINARY_DIR}/include"
    -directory.
    
    Othwerwise CMAKE_CURRENT_BINARY_DIR points to the component name
    directory and camkes.h is created to component instance name directory.
    
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: Id548b81aeac4ee1de63c7aef84988c40b53c0161

--- teeos_components/ree_comm/ree_comm.c
+++ teeos_components/ree_comm/ree_comm.c
@@ -8,14 +8,84 @@
 #include <string.h>
 
 #include <camkes.h>
-#include <sel4/simple_types.h>
+#include <camkes/io.h>
+#include <camkes/irq.h>
+#include <camkes/dataport.h>
+#include <camkes/dma.h>
 #include <utils/zf_log.h>
 #include <utils/zf_log_if.h>
 
-#include "ree_comm.h"
+#include <linux/dt-bindings/mailbox/miv-ihc.h>
+#include <linux/mailbox/miv_ihc_message.h>
+
+#include <rpmsg_platform.h>
+#include <rpmsg_sel4.h>
 #include "ree_comm_defs.h"
 
-static struct ree_comm_lib_cfg lib_cfg;
+struct camkes_app_ctx {
+    ps_io_ops_t ops;
+
+    struct sel4_rpmsg_config rpmsg_cfg;
+};
+
+static struct camkes_app_ctx app_ctx;
+
+static int setup_rpsmg(struct sel4_rpmsg_config *cfg)
+{
+    int err = -1;
+
+    /* IHC memory allocation */
+    cfg->ihc_buf_va = camkes_dma_alloc(sizeof(struct ihc_sbi_msg), 0, 0);
+    if (!cfg->ihc_buf_va) {
+        ZF_LOGF("camkes_dma_alloc error");
+        return -ENOMEM;
+    }
+
+    cfg->ihc_buf_pa = camkes_dma_get_paddr(cfg->ihc_buf_va);
+    if (!cfg->ihc_buf_pa) {
+        ZF_LOGF("camkes_dma_get_paddr error");
+        return -EIO;
+    }
+
+    /* Create RPMSG remote endpoint and wait for master to come online */
+    err = rpmsg_create_sel4_ept(cfg);
+    if (err) {
+        return err;
+    }
+
+    /* Announce RPMSG TTY endpoint to linux */
+    err = rpmsg_announce_sel4_ept();
+    if (err) {
+        return err;
+    }
+
+    return err;
+}
+
+static int wait_ree_rpmsg_msg()
+{
+    int err = -1;
+    char *msg = NULL;
+    uint32_t msg_len = 0;
+
+    while (1) {
+        ZF_LOGI("waiting REE msg...");
+
+        /* function allocates memory for msg */
+        err = rpmsg_wait_ree_msg(&msg, &msg_len);
+        if (err) {
+            ZF_LOGE("ERROR rpmsg_wait_ree_msg: %d", err);
+            continue;
+        }
+
+        ZF_LOGI("REE msg: %d", msg_len);
+
+        free(msg);
+        msg = NULL;
+    }
+
+    return err;
+}
 
 /* function prototype aligned with seL4_Wait() */
 static void tty_notify_wait(seL4_CPtr src __attribute__((unused)),
@@ -30,15 +100,6 @@ static seL4_Error tty_irq_handler_ack(seL4_IRQHandler _service __attribute__((un
     return tty_irq_acknowledge();
 }
 
-void pre_init(void)
-{
-    lib_cfg.rpmsg_cfg.irq_handler_ack = tty_irq_handler_ack;
-    lib_cfg.rpmsg_cfg.irq_notify_wait = tty_notify_wait;
-    lib_cfg.rpmsg_cfg.vring_va = rpmsg_buf;
-    lib_cfg.rpmsg_cfg.vring_pa = RPMSG_BUFFER_PADDR;
-    lib_cfg.crashlog_buf = crashlog_buf;
-}
-
 void tty_irq_handle(void)
 {
     ZF_LOGI("tty_irq_handle");
@@ -46,7 +107,27 @@ void tty_irq_handle(void)
     rpmsg_ntf_source_emit();
 }
 
-int run(void)
+int run() 
 {
-    return ree_comm_run(&lib_cfg);
+    int err = -1;
+
+    ZF_LOGI("started: ree_comm");
+
+    err = camkes_io_ops(&app_ctx.ops);
+    if (err) {
+        ZF_LOGF("camkes_io_ops: %d", err);
+        return err;
+    }
+
+    app_ctx.rpmsg_cfg.irq_handler_ack = tty_irq_handler_ack;
+    app_ctx.rpmsg_cfg.irq_notify_wait = tty_notify_wait;
+    app_ctx.rpmsg_cfg.vring_va = rpmsg_buf;
+    app_ctx.rpmsg_cfg.vring_pa = RPMSG_BUFFER_PADDR;
+
+    err = setup_rpsmg(&app_ctx.rpmsg_cfg);
+    if (err) {
+        return err;
+    }
+
+    return wait_ree_rpmsg_msg();
 }
