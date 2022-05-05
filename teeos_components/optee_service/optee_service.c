718d589029d0ca80ac15b3512f13e65721c2ee38
Tero Salminen <tero.salminen@unikie.com>
2022-05-05 10:30:06 +0300

    Add camkes optee_service component
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I685d58815f9615c86178cb28583034ce3c710f96

---
+++ teeos_components/optee_service/optee_service.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2022, Unikie
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <camkes.h>
+#include <camkes/io.h>
+#include <camkes/irq.h>
+#include <camkes/dataport.h>
+#include <camkes/dma.h>
+#include <utils/zf_log.h>
+#include <utils/zf_log_if.h>
+#include <utils/debug.h>
+
+int run(void)
+{
+    ZF_LOGI("started: optee_service");
+
+    return 0;
+}
